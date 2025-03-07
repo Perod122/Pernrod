0662d03a5cc656d0f77c55cace9115d7850dfe4f
Perod122 <jasperballesteros21@gmail.com>
2025-03-07 22:33:59 +0800

    arc jet + rate limiting

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,26 @@
+import arcjet, {tokenBucket,shield,detectBot} from "@arcjet/node";
+
+import "dotenv/config";
+export const aj = arcjet({
+    key: process.env.ARCJET_KEY,
+    characteristics:["ip.src"],
+    rules: [
+        //sheild protects the server from DDoS attacks
+        shield({mode:"LIVE"}),
+    
+    detectBot({
+        //block all bots except search engines
+        mode:"LIVE",
+        allow:["CATEGORY:SEARCH_ENGINE"
+        // see full list at https://arcjet.io/docs/bot-detection/bot-categories
+        ],
+    }),
+    //rate limiting
+    tokenBucket({
+        mode:"LIVE",
+        refillRaterate: 5,
+        intervalL: 10,
+        capacity: 10,
+        }),
+    ]
+});
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
@@ -18,6 +19,33 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet()); //helmet is a security middleware that helps you protect your app by setting various HTTP headers
 app.use(morgan("dev")); // this will log the request
+
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+    try {
+        const decision = await aj(req,{
+            requested:1
+            }    
+        );
+        if(decision.isDenied()){
+            if(decision.isRateLimit()){
+                return res.status(429).json({
+                    error:"Too many requests"});
+            } else if(decision.isBot()){
+                return res.status(403).json({
+                    error:"You are a bot"
+                });
+            } else {
+                return res.status(403).json({
+                    error:"You are not allowed to access this resource"
+                });
+            }
+        }
+    } catch (error) {
+        
+    }
+
+});
 
 app.use("/api/products", productRoutes);
 
